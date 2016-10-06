1fe1a9bbc4180996573115fcd5542d412ff69dd4
Vinh Nguyen <kurei@axcoto.com>
2016-10-05 21:10:13 -0700

    Initial Commit

---
+++ SocialPodcast/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SocialPodcast
+//
+//  Created by Vinh Nguyen on 10/5/16.
+//  Copyright © 2016 Axcoto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
