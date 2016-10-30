1cdac1063f546175f2d63bd6b822b6ecaef35419
Vinh Nguyen <kurei@axcoto.com>
2016-10-29 23:27:24 -0700

    Task async

--- server/super_tiger/lib/crawler/itunes/home.ex
+++ server/super_tiger/lib/crawler/itunes/home.ex
@@ -1,18 +1,21 @@
 defmodule SuperTiger.Crawler.Itunes.Home do
   def get_category do
     HTTPoison.start
+    task = Task.async(fn -> process_category("https://itunes.apple.com/us/genre/podcasts/id26?mt=2", 1) end)
+    Task.await(task)
+  end
 
-    IO.puts "Get index"
-    case HTTPoison.get "https://itunes.apple.com/us/genre/podcasts/id26?mt=2" do
+  def get_url(url) do
+    case HTTPoison.get url do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        process_sitemap(body)
+        body
       _ ->
-        IO.puts "Unexpected error"
+        raise "Fail to get url #{:url}. Unexpected error"
     end
   end
 
-  def process_sitemap(body) do
-    body
+  def process_category(url, parent_id) do
+    get_url(url)
     |> Floki.find("#genre-nav .top-level-genre")
     |> Enum.map(fn(item) ->
         url = Floki.attribute(item, "href") |> List.first
@@ -22,10 +25,14 @@ defmodule SuperTiger.Crawler.Itunes.Home do
           :id => id["id"],
           :name => Floki.text(item),
           :url  => url,
+          :parent_id => parent_id
         }
 
         IO.inspect category
-
+        Task.async(fn() ->
+          process_category(url, category[:id])
         end)
+      end)
+    |> Enum.map(fn(t) -> IO.inspect t; Task.await(t) end)
   end
 end
